6d21126ba562880544de6ad10bfc934cc7e799e7
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-26 11:58:36 -0500

    Linkedin Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header.jsx';
 
 function App() {
   return (
     <div className="app">
-     {/*Header*/}
+     <Header></Header>
      {/*App Body*/}
      {/*Sidebar*/}
      {/*Feed*/}
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../views/Header.css";
+
+function Header(){
+    return <div className="header">
+        <div className="header__left">
+            <img></img>
+            <div className="header__search"></div>
+        </div>
+        <div className="header__right"></div>
+    </div>
+}
+export default Header

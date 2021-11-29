2a07a24fa887503c6d3073a8a849d4275315ac47
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-29 10:54:57 -0500

    Added Linkedin Logo in Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import "../views/Header.css";
+import SearchIcon from '@mui/icons-material/Search';
 
 function Header(){
     return <div className="header">
         <div className="header__left">
-            <img></img>
-            <div className="header__search"></div>
+            <img src="linkedin.svg" alt=""></img>
+            <div className="header__search">
+                {/*SearchIcon*/}
+                <SearchIcon></SearchIcon>
+                <input type="text"></input>
+            </div>
         </div>
         <div className="header__right"></div>
     </div>

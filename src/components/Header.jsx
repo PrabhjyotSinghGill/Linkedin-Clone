07c12b543c48914a985717c09ecc4553f5a39ec9
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-29 16:38:25 -0500

    Working on Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import "../views/Header.css";
+import HeaderOption from './HeaderOption.jsx';
 import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 
 function Header(){
     return <div className="header">
@@ -12,7 +15,10 @@ function Header(){
                 <input type="text"></input>
             </div>
         </div>
-        <div className="header__right"></div>
+        <div className="header__right">
+            <HeaderOption Icon={HomeIcon} title="Home"></HeaderOption>
+            <HeaderOption Icon={SupervisorAccountIcon} title="My Network"></HeaderOption>
+        </div>
     </div>
 }
 export default Header
---
+++ src/components/HeaderOption.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../views/HeaderOption.css"
+
+function HeaderOption({Icon, title}){
+    return (<div className="headerOption">
+        {Icon && <Icon className="headerOption__icon"></Icon>}
+        <h3 className="headerOption__title">{title}</h3>
+    </div>);
+}
+
+export default HeaderOption;

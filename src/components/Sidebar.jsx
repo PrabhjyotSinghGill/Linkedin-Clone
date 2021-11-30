c4d0953150c8962bf6fca6de65fa44924895ba32
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-30 10:30:18 -0500

    Testing Complete

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,6 +3,6 @@ import "../views/Sidebar.css";
 
 function Sidebar (){
 
-    return(<div className="sidebar">Sidebar git push check</div>);
+    return(<div className="sidebar">Sidebar</div>);
 }
 export default Sidebar
